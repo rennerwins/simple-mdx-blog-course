7733147ce34c16f9418210ac210d4c22c6a215e7
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2020-05-06 23:40:47 +0700

    added feature image

--- src/elements/NavElements.js
+++ src/elements/NavElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const NavWrapper = styled.nav`
   grid-column: 2 / span 12;
-  grid-row: 1 / span 2;
+  grid-row: 1 / 2;
   display: flex;
   align-items: center;
 
