ece7ae93d7f450ac77107fb5952854e7f2a42d10
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2020-05-06 18:03:38 +0700

    add styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,26 @@
+export default {
+  fonts: {
+    main: 'Muli, sans-serif',
+    code: 'Roboto Mono, monospace',
+  },
+  breakpoints: {
+    mobile: 'only screen and (max-width: 50rem)',
+    tablet: 'only screen and (max-width: 65rem)',
+  },
+  spacings: {
+    xxSmall: '.25rem',
+    xSmall: '.5rem',
+    small: '1rem',
+    medium: '2rem',
+    large: '3rem',
+    xLarge: '4rem',
+    xxLarge: '6rems',
+  },
+  animations: {
+    button: 'box-shadow 0.3s ease',
+    link: 'color 0.2s ease',
+  },
+  shadows: {
+    shadow1: '0px 5px 20px rgba(30, 30, 31, 0.05)',
+  },
+}
