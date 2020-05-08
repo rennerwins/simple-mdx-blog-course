16347014bd2b4296ad825f0e8d990c0fee768c4c
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2020-05-08 23:16:15 +0700

    create card content component

--- src/elements/ContentElements.js
+++ src/elements/ContentElements.js
@@ -3,16 +3,18 @@ import styled from 'styled-components'
 export const ContentWrapper = styled.main`
   grid-column: 4 / span 8;
   grid-row: 3 / span 5;
-  background-color: ${({ theme: colors }) => colors.light2};
-  padding: ${({ theme: spacings }) => `${spacings.xLarge} ${spacings.xxLarge}`};
-  box-shadow: ${({ theme: shadows }) => shadows.shadow1};
+  background-color: ${props => props.theme.colors.light2};
+  padding: ${props =>
+    `${props.theme.spacings.xLarge} ${props.theme.spacings.xxLarge}`};
+  box-shadow: ${props => props.theme.shadows.shadow1};
   z-index: 10;
 
-  @media ${({ theme: breakpoints }) => breakpoints.tablet} {
+  @media ${props => props.theme.breakpoints.tablet} {
     grid-column: 2 / span 6;
   }
 
-  @media ${({ theme: breakpoints }) => breakpoints.mobile} {
-    padding: ${({ theme: spacings }) => `${spacings.medium} ${spacings.large}`};
+  @media ${props => props.theme.breakpoints.mobile} {
+    padding: ${props =>
+      `${props.theme.spacings.medium} ${props.theme.spacings.large}`};
   }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Container, FeatureImage } from '../components'
+import { Container, FeatureImage, Content, ContentCard } from '../components'
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="March 22, 2020"
+          title="Bring Football Back"
+          excerpt="Beef capicola shank chuck."
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
